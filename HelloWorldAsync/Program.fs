6bc95400f257a32d9cae403018ed36e0cb77d9d6
Piotr Justyna <piotr.justyna@gmail.com>
2021-11-10 13:15:14 +0000

    Demonstrated cancellation tokens.

--- HelloWorldAsync/Program.fs
+++ HelloWorldAsync/Program.fs
@@ -1,19 +1,76 @@
-open System.IO
+open System.Net
+open System.Threading
+open FSharp.Data
 
-let printTotalFileBytes path =
+// 2021-11-10 PJ:
+// --------------
+// The was taken from this paper: https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/async-padl-revised-v2.pdf
+// However, I refactored it slightly as some dependencies got decommissioned/archived since the article publication date.
+// To keep things simple, I replaced all the string-retrieving lines with one line leveraging the FSharp.Data nuget.
+// More here: http://fsprojects.github.io/FSharp.Data/library/Http.html
+let getWebPage (url: string) =
     async {
-        let! bytes = File.ReadAllBytesAsync(path) |> Async.AwaitTask
-
-        let fileName = Path.GetFileName(path)
-
-        printfn $"File {fileName} has %d{bytes.Length} bytes"
+        let! response = Http.AsyncRequestString(url)
+        printfn $"content length: %d{response.Length} characters"
     }
+
+// 2021-11-10 PJ:
+// --------------
+// The work gets cancelled immediately.
+let scenario1 () =
+    let capability = new CancellationTokenSource()
+
+    // 2021-11-10 PJ:
+    // --------------
+    // There is an error in the paper. Async.Start cannot accept Action<unit []> (result of Async.Parallel).
+    // It can only take one asynchronous expression of type Async<unit>.
+    Async.Start(getWebPage "http://www.google.com", capability.Token)
+
+    // 2021-11-10 PJ:
+    // --------------
+    // Cancel immediately. We know the work has not finished yet.
+    // It would probably take at least a few ms to finish.
+    capability.Cancel()
+
+    printfn "scenario1 finished"
+
+// 2021-11-10 PJ:
+// --------------
+// The work does not get cancelled.
+let scenario2 () =
+    let capability = new CancellationTokenSource()
+    
+    Async.Start(getWebPage "http://www.google.com", capability.Token)
+
+    // 2021-11-10 PJ:
+    // --------------
+    // Dummy wait to make sure the request finishes.
+    Async.RunSynchronously(async { do! Async.Sleep(5000) })
+
+    printfn "scenario2 finished"
+
+// 2021-11-10 PJ:
+// --------------
+// The work does get cancelled, but only after 50ms.
+// The purpose of this is to demonstrate that even when we have the dummy 5000ms wait,
+// meaning: we have the time to wait for the external call,
+// the underlying "getWebPage" operation gets cancelled successfully anyway.
+// I would encourage readers to experiment with longer CancellationTokenSource
+// timespans: 3s, 4s, etc. (anything below 5s - the dummy wait).
+let scenario3 () =
+    let capability =
+        new CancellationTokenSource(System.TimeSpan.FromMilliseconds(50.0))
+
+    Async.Start(getWebPage "http://www.google.com", capability.Token)
+
+    // 2021-11-10 PJ:
+    // --------------
+    // Dummy wait to make sure the request finishes.
+    Async.RunSynchronously(async { do! Async.Sleep(5000) })
+
+    printfn "scenario3 finished"
 
 [<EntryPoint>]
 let main argv =
-    argv
-    |> Seq.map printTotalFileBytes
-    |> Async.Sequential
-    |> Async.Ignore
-    |> Async.RunSynchronously
+    scenario1 ()
     0
